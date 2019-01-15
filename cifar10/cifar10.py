21cc543cd8fe4a22ea933294b052a6ea5dea965e
Wonderful23 <43582078+Wonderful23@users.noreply.github.com>
2019-01-15 09:41:28 +0800

    修改一些错误
    修正了其中的一些错误，现在这个python文件可以运行

--- cifar10/cifar10.py
+++ cifar10/cifar10.py
@@ -5,48 +5,45 @@ import time
 max_steps = 3000
 batch_size = 128
 data_dir = '/tmp/cifar10_data/cifar-10-batches-bin'
-
-def variable_with_weight(shape,stddev,w1):
+def variable_with_weight_loss(shape,stddev,w1):
     var = tf.Variable(tf.truncated_normal(shape,stddev=stddev))
     if w1 is not None:
         weight_loss = tf.multiply(tf.nn.l2_loss(var),w1,name="weight_loss")
         tf.add_to_collection('losses',weight_loss)
     return var
-
 cifar10.maybe_download_and_extract()
 images_train,labels_train = cifar10_input.distorted_inputs(data_dir=data_dir,batch_size=batch_size)
 images_test,labels_test = cifar10_input.inputs(eval_data=True,data_dir=data_dir,batch_size=batch_size)
 image_holder = tf.placeholder(tf.float32,[batch_size,24,24,3])
 label_holder = tf.placeholder(tf.int32,[batch_size])
-#第一卷积层
 weight1 = variable_with_weight_loss(shape=[5,5,3,64],stddev=5e-2,w1=0.0)
 kernel1 = tf.nn.conv2d(image_holder,weight1,[1,1,1,1],padding='SAME')
 bias1 = tf.Variable(tf.constant(0.0,shape=[64]))
 conv1 = tf.nn.relu(tf.nn.bias_add(kernel1,bias1))
 pool1 = tf.nn.max_pool(conv1,ksize=[1,3,3,1],strides=[1,2,2,1],padding='SAME')
 norm1 = tf.nn.lrn(pool1,4,bias=1.0,alpha=0.001/9.0,beta=0.75)
-#第二卷积层
+
 weight2 = variable_with_weight_loss(shape=[5,5,64,64],stddev=5e-2,w1=0.0)
 kernel2 = tf.nn.conv2d(norm1,weight2,[1,1,1,1],padding='SAME')
 bias2 = tf.Variable(tf.constant(0.1,shape=[64]))
 conv2 = tf.nn.relu(tf.nn.bias_add(kernel2,bias2))
 norm2 = tf.nn.lrn(conv2,4,bias=1.0,alpha=0.001/9.0,beta=0.75)
 pool2 = tf.nn.max_pool(norm2,ksize=[1,3,3,1],strides=[1,2,2,1],padding='SAME')
-#第一全连接层
+
 reshape = tf.reshape(pool2,[batch_size,-1])
 dim = reshape.get_shape()[1].value
 weight3 = variable_with_weight_loss(shape=[dim,384],stddev=0.04,w1=0.004)
 bias3 = tf.Variable(tf.constant(0.1,shape=[384]))
 local3 = tf.nn.relu(tf.matmul(reshape,weight3)+bias3)
-#第二全连接层
+
 weight4 = variable_with_weight_loss(shape=[384,192],stddev=0.04,w1=0.004)
 bias4 = tf.Variable(tf.constant(0.1,shape=[192]))
 local4 = tf.nn.relu(tf.matmul(local3,weight4)+bias4)
-#interferce交互的结果
+
 weight5 = variable_with_weight_loss(shape=[192,10],stddev=1/192.0,w1=0.0)
 bias5 = tf.Variable(tf.constant(0.0,shape=[10]))
 logits = tf.add(tf.matmul(local4,weight5),bias5)
-#定义权重损失
+
 def loss(logits,labels):
     labels = tf.cast(labels,tf.int64)
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=labels,name='cross_entropy_per_example')
@@ -56,9 +53,9 @@ def loss(logits,labels):
 
 loss = loss(logits,label_holder)
 train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
-top_k_op = tf.nn_in_top_k(logits,label_holder,1)
+top_k_op = tf.nn.in_top_k(logits,label_holder,1)
 sess = tf.InteractiveSession()
-tf.global_variable_initializer().run()
+tf.global_variables_initializer().run()
 tf.train.start_queue_runners()
 
 for step in range(max_steps):
@@ -70,7 +67,7 @@ for step in range(max_steps):
         examples_per_sec = batch_size/duration
         sec_per_batch =  float(duration)
         format_str =('step %d,loss = %.2f(%.1f examples/sec;%.3f sec/batch)')
-        print(format_str %(step,loss_value,example_per_sec,sec_per_batch))
+        print(format_str %(step,loss_value,examples_per_sec,sec_per_batch))
 
 num_examples = 10000
 import math
@@ -85,26 +82,3 @@ while step < num_iter:
     step+=1
 precision =true_count/total_sample_count
 print('precision @ 1 = %.3f'%precision)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
